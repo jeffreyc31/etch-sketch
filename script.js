2e098ff56f7bb7c37729e14bf141de0acedd0549
jeffreyc31 <jeffcgit31@gmail.com>
2022-03-08 12:25:03 -0800

    Add event listener to each box to change color to black
    The event listener is added to each box with a forEach loop cycling
    through an array of div elements from the boxes class. The event
    listener calls on the changeColor function upon the mouse hovering over
    the div, which changes background color to black. The array was created
    from the nodeList of the boxes class.

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for (i = 1; i <= 16; i++) {
 
     container.appendChild(newDiv);
 }
+
+function changeColor() {
+    this.style.backgroundColor = "black";
+    return false;
+}
+
+let boxList = document.querySelectorAll('div.boxes');
+const boxArray = Array.from(boxList);
+
+boxArray.forEach(element => element.addEventListener("mouseover", changeColor));
