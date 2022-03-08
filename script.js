3987c59f0f31ac64b60fd29e518cdaa527eab1fc
jeffreyc31 <jeffcgit31@gmail.com>
2022-03-08 13:46:59 -0800

    Start features of reset button
    The reset button will prompt the user to enter a whole number 1-100 and
    create a grid using the number entered (e.g. entering 12 would make a
    12x12 grid). The grid will take up the same amount of space as the
    initial.
    
    Currently, it has code to filter out non-numbers, floats, and numbers
    outside of the specified range, but other functionality is not finished.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Script for initial page 16x16 grid
+//variable and function declaration
 
 let newDiv;
 
@@ -22,4 +22,31 @@ function colorBox() {
 
 }
 
+//Script for initial page 16x16 grid
+
 colorBox();
+
+//script to reset grid
+
+function getSize() {
+    let answer = prompt("Select a whole number 1-100 to set as the height and width of your grid.");
+    return answer;
+}
+
+let reset = document.getElementById('reset');
+reset.addEventListener('click', function(e) {
+    let size = getSize();
+    let sizeType = Number(size);
+    let divWidth = 680 / size;
+    let divHeight = 600 / size;
+    console.log(size);
+    console.log(sizeType);
+
+    if (sizeType == NaN) {
+        alert("Your entry was not a valid number. Please press the 'Reset' button and try again.")
+    } else if ((size % 1) != 0) {
+        alert("Your entry was not a valid number. Please press the 'Reset' button and try again.")
+    } else if (size < 0 || size > 100) {
+        alert("Your entry was not a valid number. Please press the 'Reset' button and try again.")
+    }
+});
