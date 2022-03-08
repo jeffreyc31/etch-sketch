2179212e945c2d6d371d765b5b4f96eeaa40a669
jeffreyc31 <jeffcgit31@gmail.com>
2022-03-08 14:08:17 -0800

    styles.css: Change padding and margin to 0
    Remove width and height
    
    script.js:
    Add setDimension function that sets initial and future grid dimensions
    
    Button deletes initial grid and creates grid with user-inputted number

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //variable and function declaration
 
 let newDiv;
+let divHeight = 150;
+let divWidth = 150;
 
 for (i = 1; i <= 16; i++) {
     newDiv = document.createElement('div');
@@ -19,26 +21,33 @@ function colorBox() {
     let boxList = document.querySelectorAll('div.boxes');
     const boxArray = Array.from(boxList);
     boxArray.forEach(element => element.addEventListener("mouseover", changeColor));
-
 }
-
-//Script for initial page 16x16 grid
-
-colorBox();
-
-//script to reset grid
 
 function getSize() {
     let answer = prompt("Select a whole number 1-100 to set as the height and width of your grid.");
     return answer;
 }
 
+function setDimension() {
+    let boxList = document.querySelectorAll('div.boxes');
+    const boxArray = Array.from(boxList);
+    boxArray.forEach(element => element.setAttribute(`style`, `width:${divWidth}px; height:${divHeight}px`));
+}
+
+//Script for initial page 16x16 grid
+
+setDimension();
+colorBox();
+
+
+//script to reset grid
+
 let reset = document.getElementById('reset');
 reset.addEventListener('click', function(e) {
     let size = getSize();
     let sizeType = Number(size);
-    let divWidth = 680 / size;
-    let divHeight = 600 / size;
+    divWidth = 680 / size;
+    divHeight = 600 / size;
     console.log(size);
     console.log(sizeType);
 
@@ -48,5 +57,22 @@ reset.addEventListener('click', function(e) {
         alert("Your entry was not a valid number. Please press the 'Reset' button and try again.")
     } else if (size < 0 || size > 100) {
         alert("Your entry was not a valid number. Please press the 'Reset' button and try again.")
+    } else {
+        const emptyCont = document.getElementById('container');
+        while (emptyCont.firstChild) {
+            emptyCont.removeChild(emptyCont.lastChild);
+        }
+
+        for (i = 1; i <= size; i++) {
+            newDiv = document.createElement('div');
+            newDiv.id = `box-${i}`;
+            newDiv.classList.add("boxes");
+
+            container.appendChild(newDiv);
+        }
+        colorBox();
+        setDimension();
+
     }
+
 });
